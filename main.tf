a1be7d48b9facb8682185ee2a41a5b9dea425a23
TTTTTTTT <TTTTTTTT@example.com>
2026-03-19 12:01:26 +0700

    add: allow-permissions

--- main.tf
+++ main.tf
@@ -24,6 +24,8 @@ module "eks" {
     cluster_name    = "sre-demo"
     cluster_version = "1.32"
 
+    enable_cluster_creator_admin_permissions = true
+
     vpc_id     = module.vpc.vpc_id
     subnet_ids = module.vpc.public_subnets
 
